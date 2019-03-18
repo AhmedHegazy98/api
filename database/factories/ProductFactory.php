f58346b807f3f81cafde6c94c64441817a76a53e
AhmedHegazy98 <themastaer98@gmail.com>
2019-03-18 03:40:05 -0700

    Seeding part for product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,14 @@
 <?php
 
 use Faker\Generator as Faker;
-
+// you can visit the documentaion on github and laravel docs
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100,1000),
+        'stock'=>$faker->randomDigit,
+        'discount'=>$faker->numberBetween(2,30),
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\Model\Product;
+// you can visit the documentaion on github and laravel docs
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=>function()
+        {
+            return Product::all()->random();
+        },
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,5),
+        
     ];
 });
