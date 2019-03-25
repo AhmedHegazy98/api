99ae30761c7a7049a3fb8593127f0a1ddc986f23
AhmedHegazy98 <themastaer98@gmail.com>
2019-03-25 04:59:49 -0700

    Product Delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -106,6 +106,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null,204);
     }
 }
