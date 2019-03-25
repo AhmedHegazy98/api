37710fe92618715b4a42119528204698e9d60872
AhmedHegazy98 <themastaer98@gmail.com>
2019-03-25 08:39:12 -0700

    Customised Exception Trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Exceptions;
+use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+trait ExceptionTrait{
+
+    public function apiException($request,$e)
+    {
+        
+        if ($this->isModel($e))
+        {
+            return response()->json([
+                'errors'=>'Product Model not found'
+            ],404);
+        }
+
+    if ($e instanceof NotFoundHttpException) //can make is Http Func Like Model see Commit""
+        {
+            return response()->json([
+                'errors'=>'Incorrect Route'
+            ],404);
+        }
+
+        return parent::render($request, $exception);
+      
+    }
+
+    protected function isModel($e)
+    {
+        return $e instanceof ModelNotFoundException;
+    }
+}
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,12 +4,11 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -51,21 +50,9 @@ class Handler extends ExceptionHandler
     {
         if($request->expectsJson()){
             
-            if ($exception instanceof ModelNotFoundException)
-                {
-                    return response()->json([
-                        'errors'=>'Product Model not found'
-                    ],404);
-                }
-
-            if ($exception instanceof NotFoundHttpException)
-                {
-                    return response()->json([
-                        'errors'=>'Incorrect Route'
-                    ],404);
-                }
+           return $this->apiException($request,$exception);
         }
         
-        return parent::render($request, $exception);
+        
     }
 }
